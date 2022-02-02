449d87200ab21cda051c5de7b438d924e1ed5f08
Alana Gilston <abluescarab@users.noreply.github.com>
2022-02-02 01:29:42 -0800

    Updated gitignore

--- CS333/Lab3/lab3.c
+++ CS333/Lab3/lab3.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 #include "lab3.h"
 
 #define CHAR_LIMIT 4096
+#define FALSE 0
+#define TRUE 1
 
 int main() {
     char input[CHAR_LIMIT];
     char * result[CHAR_LIMIT];
     int index = 0;
-    int quit = 0;
 
     signal(SIGCHLD, handle_sigchld);
 
-    while(!quit) {
+    while(TRUE) {
+        printf("sillyshell$ ");
+
+        // ensure that fgets gives valid input
         while(fgets(input, CHAR_LIMIT, stdin) == NULL) {
             fflush(stdin);
-            printf("sillyshell$ ");
         }
-        // fgets(input, CHAR_LIMIT, stdin);
 
+        // remove the new line character
+        input[strlen(input) - 1] = '\0';
         index = convert_to_tokens(input, result);
 
-        printf("%s", result[index - 1]);
+        printf("%d\n", index);
 
-        if(*result[index - 1] == '&') {
-            printf("yes\n");
+        if(strcmp(result[0], "exit") == 0) {
+            exit(0);
         }
 
-        // implement support for exit
-        exit(0);
+        // parse the input, setting in_background to whether the last token was &
+        parse_input(result, *result[index - 1] == '&');
     }
 
     return 0;
 }
 
+// modified from https://stackoverflow.com/a/10349393/567983
 int convert_to_tokens(char * input, char ** result) {
     char * token;
     int index = 0;
@@ -55,8 +61,12 @@ int convert_to_tokens(char * input, char ** result) {
     return index;
 }
 
-int parse_input(char ** input) {
+int parse_input(char ** input, int in_background) {
     // TODO
+    if(in_background == TRUE) {
+        printf("yes");
+    }
+
     return 0;
 }
 
--- CS333/Lab3/lab3.h
+++ CS333/Lab3/lab3.h
@@ -2,7 +2,7 @@
 #define LAB3_H
 
 int convert_to_tokens(char *, char **);
-int parse_input(char **);
+int parse_input(char **, int);
 void handle_sigchld(int);
 
 #endif
