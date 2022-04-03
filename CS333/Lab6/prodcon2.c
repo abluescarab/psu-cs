43ac2658b947570f85ba706f748768dd29625ba3
Alana Gilston <abluescarab@users.noreply.github.com>
2022-04-02 21:18:47 -0700

    Removed unused Lab6

--- CS333/Lab6/prodcon2.c
+++
@@ -1,9 +0,0 @@
-// ********************************************************
-// Name:       Alana Gilston
-// Class:      CS333
-// Assignment: Lab 6
-// File:       prodcon2.c
-// ********************************************************
-int main(int argc, char *argv[]) {
-    return 0;
-}
--- CS333/Lab6/prodcon3.c
+++
@@ -1,9 +0,0 @@
-// ********************************************************
-// Name:       Alana Gilston
-// Class:      CS333
-// Assignment: Lab 6
-// File:       prodcon3.c
-// ********************************************************
-int main(int argc, char *argv[]) {
-    return 0;
-}
