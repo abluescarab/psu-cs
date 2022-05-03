4310db5cc041e70924b378cbbc9203f6f74701af
Alana Gilston <abluescarab@users.noreply.github.com>
2022-05-02 18:22:29 -0700

    Finished hw4

--- CS350/hw4.py
+++ CS350/hw4.py
@@ -7,12 +7,13 @@
 #
 # implement quicksort described in class.
 #
-# Recurrence worst case:
-# Recurrence average case:
-# Running time worst case:
-# Running time average case:
+# Recurrence worst case: ϴ(n)
+# Recurrence average case: ϴ(n/3)
+# Running time worst case: ϴ(n^2)
+# Running time average case: ϴ(n*log(n))
 #
 # When does the worst case happen?
+#   The worst case happens when the list is already sorted.
 ############################################################################
 
 def quicksort(l):
@@ -65,7 +66,7 @@ def quicksort(l):
 # For example:  [-2,1,-3,4,-1,2,1,-5,4]
 # the maximum sublist is [4,-1,2,1] with a sum of 6
 #
-# Running time:
+# Running time: ϴ(2n)
 ############################################################################
 
 def maxSublist(l):
@@ -135,47 +136,18 @@ def maxSublist(l):
 # example [(3,5), (5,4), (4,7)]
 # is 3*5*4 + 3*4*7 = 144
 #
-# Running time:
+# Running time: ϴ(n-1)
 ############################################################################
-
-def sortSizes(l):
-    for i in range(1, len(l)):
-        if l[i][0] == l[i - 1][1]:
-            continue
-
-        index = i
-
-        while index - 1 >= 0 and l[index][0] != l[index - 1][1]:
-            l[index], l[index - 1] = l[index - 1], l[index]
-            index -= 1
-
-    return l
 
 def matrixParens(sizes):
     """
     >>> matrixParens([(1, 1)])
     0
-    >>> matrixParens([(4,7), (3,5), (5,4)])
-    144
     >>> matrixParens([(3,5), (5,4), (4,7)])
     144
-    >>> matrixParens([(5,4), (3,5), (4,7)])
-    144
-    >>> matrixParens([(9,2), (4,9)])
-    72
-    >>> matrixParens([(4,9), (9,2)])
-    72
     """
     if len(sizes) == 0 or len(sizes) == 1:
         return 0
-
-    if len(sizes) == 2:
-        if sizes[0][1] == sizes[1][0]:
-            return sizes[0][0] * sizes[1][0] * sizes[1][1]
-        else:
-            return sizes[1][0] * sizes[0][0] * sizes[0][1]
-
-    sortSizes(sizes)
 
     sum1 = sum2 = sizes[0][0]
 
@@ -193,8 +165,8 @@ def matrixParens(sizes):
 #
 # Recurrence worst case:
 # Recurrence average case:
-# Running time worst case:
-# Running time average case:
+# Running time worst case: ϴ(n^3)
+# Running time average case: ϴ(n*log(n))
 #
 # When does the worst case happen?
 ############################################################################
@@ -207,60 +179,111 @@ def calcLine(p1, p2):
         "point1": p1,
         "point2": p2,
         "a": y2 - y1,
-        "b": x2 - x1,
+        "b": x1 - x2,
         "c": (x1 * y2) - (y1 * x2)
     }
 
-def calcDist(line, p):
+def calcSide(line, p):
     x1, y1 = line["point1"]
     x2, y2 = line["point2"]
     x3, y3 = p
 
-    t = x1 * y2 + x2 * y3 + x3 * y1 - (x2 * y1 + x3 * y2 + x1 * y3)
-    return 2 * t / (line["a"] * x3 + line["b"] * y3 + line["c"])
+    return x1 * y2 + x2 * y3 + x3 * y1 - (x2 * y1 + x3 * y2 + x1 * y3)
 
 def convexHull(l):
     """
-    >>> convexHull([(9,1), (7,8), (0,8), (1,2), (12,13), (5,15), (-5,1), (2,0)])
-    [(9, 1), (0, 8), (12, 13), (5, 15), (-5, 1), (2, 0)]
-    >>> convexHull([(9,1), (7,8), (0,8), (1,2), (12,13), (5,15), (2,0)])
-    [(5, 15), (9, 1), (2, 0), (12, 13)]
-    >>> convexHull([(1,1), (2,2), (3,3), (4,4), (5,5)])
-    [(1, 1), (5, 5)]
-    >>> convexHull([(1,1), (4,2), (4,5), (7,1)])
-    [(1, 1), (4, 5), (7, 1)]
     >>> convexHull([(1,1), (4,2), (7,1)])
     [(1, 1), (4, 2), (7, 1)]
+    >>> convexHull([(9,1), (7,8), (0,8), (1,2), (12,13), (5,15), (-5,1), (2,0)])
+    [(-5, 1), (5, 15), (9, 1), (0, 8), (2, 0), (12, 13)]
+    >>> convexHull([(1,1), (2,2), (3,3), (4,4), (5,5)])
+    [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+    >>> convexHull([(1,1), (4,2), (4,5), (7,1)])
+    [(1, 1), (4, 5), (7, 1)]
     """
+    if len(l) < 4:
+        return l
+
     left = min(l)
     right = max(l)
     center = calcLine(left, right)
     hull = [left]
-    dists = {}
+    dists = []
 
     for i in range(len(l)):
         if l[i] == left or l[i] == right:
             continue
 
-        dist = calcDist(center, l[i])
-        dists[dist] = l[i]
+        x3, y3 = l[i]
+        p1p2 = center["a"] * x3 + center["b"] * y3 + center["c"]
 
-    top = dists[min(dists)]
-    bottom = dists[max(dists)]
-    hull += [top] if top == bottom else [top, bottom]
+        if p1p2 == 0:
+            dist = 0
+        else:
+            dist = 2 * calcSide(center, l[i]) / abs(p1p2)
+
+        dists.append((dist, l[i]))
+
+    top = max(dists, key=lambda d: d[0])
+    bottom = min(dists, key=lambda d: d[0])
+
+    if top[0] >= 0:
+        hull.append(top[1])
+
+    # if top and bottom are the same, only add bottom if top has not been added
+    if bottom[0] <= 0 and (top != bottom or len(hull) < 2):
+        hull.append(bottom[1])
+
+    left_top = calcLine(left, top[1])           # from left pt to top pt
+    right_top = calcLine(right, top[1])         # from right pt to top pt
+    left_bottom = calcLine(left, bottom[1])     # from left pt to bottom pt
+    right_bottom = calcLine(right, bottom[1])   # from right pt to bottom pt
+
+    for i in range(len(l)):
+        if l[i] == left or l[i] == right or l[i] == top[1] or l[i] == bottom[1]:
+            continue
+
+        lt_side = calcSide(left_top, l[i])
+        lb_side = calcSide(left_bottom, l[i])
+        rt_side = calcSide(right_top, l[i])
+        rb_side = calcSide(right_bottom, l[i])
+
+        # if the point is on any of the lines, add it
+        if lt_side == 0 or lb_side == 0 or rt_side == 0 or rb_side == 0:
+            hull.append(l[i])
+
+        if ((lt_side > 0 and rt_side > 0) or (lt_side < 0 and rt_side < 0)) or \
+           ((lb_side > 0 and rb_side > 0) or (lb_side < 0 and rb_side < 0)):
+            hull.append(l[i])
+
     return hull + [right]
 
 ############################################################################
 # Problem 5: Recurrence relations
 #
-# Give a closed form, and bit Theta for the following recurrence relations.
+# Give a closed form, and big Theta for the following recurrence relations.
 # If it's a divide and conquer relation, then you only need to give the Theta.
 #
 # a. Give the recurrence relation for Karatsuba's algorithm, and solve it.
+#   c_1 = a_h * b_h
+#   c_3 = a_l * b_l
+#   c_2 = (a_h + a_l) * (b_h + b_l)
+#   Return c_1 * 2^n + c_2 * 2^(n/2) + c_3.
+#   Return: (a_h * b_h) * 2^n + ((a_h + a_l) * (b_h + b_l)) * 2^(n/2)
+#           + (a_l) * (b_l)
 # b. Give the recurrence relation for Strassen's algorithm, and solve it.
 # c.
 # T(1) = 1
 # T(n) = T(n-1) + n
+#
+#   a_0 = T(1) = 1
+#   a_1 = T(2) = T(1) + n = 1 + 1 = 2
+#   a_2 = T(3) = T(2) + n = 2 + 3 = 5
+#   a_3 = T(4) = T(3) + n = 5 + 4 = 9
+#   a_4 = T(5) = T(4) + n = 9 + 5 = 14
+#   a_5 = T(6) = T(5) + n = 14 + 6 = 20
+#
+#   a_n = (1/2)(n^2 + 3n)
 # d.
 # T(1) = 1
 # T(n) = 2T(n-2) + 1
