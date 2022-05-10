a57fad3b2a611635fc0629954ef9721b7f2844e7
Alana Gilston <abluescarab@users.noreply.github.com>
2022-05-10 03:26:20 -0700

    Prelim work on hw5

--- CS350/hw5.py
+++ CS350/hw5.py
@@ -33,7 +33,24 @@ def components(g):
     >>> components([[3,4,7],[3,5,6],[4,5,7],[0,1],[0,2],[1,2],[1],[0,2]])
     [[0, 1, 2, 3, 4, 5, 6, 7]]
     """
-    pass
+    # connections = []
+
+    # for i in range(len(g)):                             # O(n)
+    #     current = set(sorted([i] + g[i]))
+    #                                                     # O(n^2)
+    #     for j in range(len(g)):                         # O(n)
+    #         if i == j:
+    #             continue
+
+    #         if any(x in current for x in g[j]):         # O(n)
+    #             current.update(g[j])
+
+    #     l = list(current)                               # O(n)
+    #                                                     # O(2n)
+    #     if l not in connections:                        # O(n)
+    #         connections.append(l)
+
+    # return connections
 
 ############################################################################
 #
@@ -61,7 +78,17 @@ def bipartite(g):
     >>> bipartite([[1,3],[0,2],[1,3,5,4],[0,2],[5,2],[2,4]])
     False
     """
-    pass
+    if len(g) == 0 or len(g[0]) == 0:
+        return True
+
+    t = set()
+    s = set()
+
+    for v in range(len(g)):
+        for w in g[v]:
+            pass
+
+    return True
 
 ############################################################################
 #
@@ -133,4 +160,17 @@ def scc(d):
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    # doctest.testmod()
+
+    import utils
+    # utils.asrt(components([[1,2], [0,2], [0,1], [4], [3]]), [[0, 1, 2], [3, 4]])
+    # utils.asrt(components([[]]), [[0]])
+    # utils.asrt(components([[1],[0]]), [[0, 1]])
+    # utils.asrt(components([[3,4,7],[3,5,6],[4,5,7],[0,1],[0,2],[1,2],[1],[0,2]]), [[0, 3, 1, 5, 2, 4, 7, 6]])
+    # utils.asrt(components([[1,2],[0,3,4],[0,6],[1],[1,8,9],[6],[5,7],[6],[4],[4],[11,12],[10,12],[10,11]]), [[0, 1, 3, 4, 8, 9, 2, 6, 5, 7], [10, 11, 12]])
+    utils.asrt(bipartite([[3,4,7], [3,5,6], [4,5,7], [0,1], [0,2], [1,2], [1], [0,2]]), True)
+    # utils.asrt(bipartite([[1,2],[0,2],[0,1]]), False)
+    # utils.asrt(bipartite([[]]), True)
+    # utils.asrt(bipartite([[1],[0]]), True)
+    # utils.asrt(bipartite([[1,3],[0,2],[1,3,5],[0,2],[5],[2,4]]), True)
+    # utils.asrt(bipartite([[1,3],[0,2],[1,3,5,4],[0,2],[5,2],[2,4]]), False)
