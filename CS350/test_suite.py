b4a17a5d237a6e1274714e33d46898871d3d72d6
Alana Gilston <abluescarab@users.noreply.github.com>
2022-05-18 23:25:09 -0700

    Made fields private; added removal functions

--- CS350/test_suite.py
+++ CS350/test_suite.py
@@ -20,35 +20,41 @@ class Test:
 
 class TestSuite:
     def __init__(self):
-        self.tests = []
-        self.performed = 0
-        self.passed = 0
+        self._tests = []
+        self._performed = 0
+        self._passed = 0
 
     def add_test(self, func, args, expected):
-        self.tests.append(Test(func, args, expected))
+        self._tests.append(Test(func, args, expected))
 
     def _print_results(self):
-        print(f"test results: {self.passed} passed, "
-              f"{self.performed - self.passed} failed, {self.performed} run")
+        if len(self._tests) > 0:
+            print()
+
+        print(f"result:", end=" ")
+
+        if len(self._tests) > 0:
+            print(f"{self._passed} passed, "
+                  f"{self._performed - self._passed} failed, {self._performed} run")
+        else:
+            print(f"no tests performed")
+
+        print()
 
     def _reset(self):
-        self.performed = 0
-        self.passed = 0
+        self._performed = 0
+        self._passed = 0
 
     def _run_single(self, test):
-        self.performed += 1
+        self._performed += 1
 
         if test.run():
-            self.passed += 1
-
-    def clear(self):
-        self.tests.clear()
-        self._reset()
+            self._passed += 1
 
     def run(self):
         self._reset()
 
-        for t in self.tests:
+        for t in self._tests:
             self._run_single(t)
 
         self._print_results()
@@ -56,7 +62,7 @@ class TestSuite:
     def run_by_function(self, func):
         self._reset()
 
-        for t in self.tests:
+        for t in self._tests:
             if t.func == func:
                 self._run_single(t)
 
@@ -65,7 +71,7 @@ class TestSuite:
     def run_by_args(self, args):
         self._reset()
 
-        for t in self.tests:
+        for t in self._tests:
             if t.args == args:
                 self._run_single(t)
 
@@ -74,8 +80,21 @@ class TestSuite:
     def run_by_expected(self, expected):
         self._reset()
 
-        for t in self.tests:
+        for t in self._tests:
             if t.expected == expected:
                 self._run_single(t)
 
         self._print_results()
+
+    def remove_by_function(self, func):
+        self._tests = [t for t in self._tests if t.func != func]
+
+    def remove_by_args(self, args):
+        self._tests = [t for t in self._tests if t.args != args]
+
+    def remove_by_expected(self, expected):
+        self._tests = [t for t in self._tests if t.expected != expected]
+
+    def clear(self):
+        self._tests.clear()
+        self._reset()
