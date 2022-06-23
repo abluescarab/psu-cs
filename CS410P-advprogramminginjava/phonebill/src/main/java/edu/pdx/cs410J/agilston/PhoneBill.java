b8406633a704309c135c141219655b8ec9b91b09
Alana Gilston <abluescarab@users.noreply.github.com>
2022-06-23 09:58:19 -0700

    Implemented PhoneBill

--- CS410P-advprogramminginjava/phonebill/src/main/java/edu/pdx/cs410J/agilston/PhoneBill.java
+++ CS410P-advprogramminginjava/phonebill/src/main/java/edu/pdx/cs410J/agilston/PhoneBill.java
@@ -2,44 +2,48 @@ package edu.pdx.cs410J.agilston;
 
 import edu.pdx.cs410J.AbstractPhoneBill;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * A class that maintains a phone bill with all its relevant phone calls.
  */
 public class PhoneBill extends AbstractPhoneBill<PhoneCall> {
-  private final String customer;
+    private final String customer;
+    private final List<PhoneCall> calls;
 
     /**
      * Creates a new phone bill.
      * @param customer customer name
      */
-  public PhoneBill(String customer) {
-    this.customer = customer;
-  }
+    public PhoneBill(String customer) {
+        this.customer = customer;
+        this.calls = new ArrayList<>();
+    }
 
     /**
      * Gets customer's name.
      */
-  @Override
-  public String getCustomer() {
-    return this.customer;
-  }
+    @Override
+    public String getCustomer() {
+        return this.customer;
+    }
 
     /**
      * Adds a phone call to the bill.
      * @param call phone call to add
      */
-  @Override
-  public void addPhoneCall(PhoneCall call) {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
+    @Override
+    public void addPhoneCall(PhoneCall call) {
+        calls.add(call);
+    }
 
     /**
      * Gets all phone calls in the bill.
      */
-  @Override
-  public Collection<PhoneCall> getPhoneCalls() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
+    @Override
+    public Collection<PhoneCall> getPhoneCalls() {
+        return calls;
+    }
 }
