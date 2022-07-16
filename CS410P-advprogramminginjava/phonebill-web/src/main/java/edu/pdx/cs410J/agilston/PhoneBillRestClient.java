344da156f26ad9db098d711998793741133598f8
Alana Gilston <abluescarab@users.noreply.github.com>
2022-07-16 02:39:50 -0700

    Finished reformatting

--- CS410P-advprogramminginjava/phonebill-web/src/main/java/edu/pdx/cs410J/agilston/PhoneBillRestClient.java
+++ CS410P-advprogramminginjava/phonebill-web/src/main/java/edu/pdx/cs410J/agilston/PhoneBillRestClient.java
@@ -22,62 +22,62 @@ public class PhoneBillRestClient {
     private static final String WEB_APP = "phonebill";
     private static final String SERVLET = "calls";
 
-  private final HttpRequestHelper http;
+    private final HttpRequestHelper http;
 
 
     /**
      * Creates a client to the Phone Bil REST service running on the given host and port
+     *
      * @param hostName The name of the host
-     * @param port The port
+     * @param port     The port
      */
-    public PhoneBillRestClient( String hostName, int port )
-    {
-      this(new HttpRequestHelper(String.format("http://%s:%d/%s/%s", hostName, port, WEB_APP, SERVLET)));
+    public PhoneBillRestClient(String hostName, int port) {
+        this(new HttpRequestHelper(String.format("http://%s:%d/%s/%s", hostName, port, WEB_APP, SERVLET)));
     }
 
-  @VisibleForTesting
-  PhoneBillRestClient(HttpRequestHelper http) {
-    this.http = http;
-  }
+    @VisibleForTesting
+    PhoneBillRestClient(HttpRequestHelper http) {
+        this.http = http;
+    }
 
-  /**
-   * Returns all dictionary entries from the server
-   */
-  public Map<String, String> getAllDictionaryEntries() throws IOException, ParserException {
-    Response response = http.get(Map.of());
+    /**
+     * Returns all dictionary entries from the server
+     */
+    public Map<String, String> getAllDictionaryEntries() throws IOException, ParserException {
+        Response response = http.get(Map.of());
 
-    TextParser parser = new TextParser(new StringReader(response.getContent()));
-    return parser.parse();
-  }
+        TextParser parser = new TextParser(new StringReader(response.getContent()));
+        return parser.parse();
+    }
 
-  /**
-   * Returns the definition for the given word
-   */
-  public String getDefinition(String word) throws IOException, ParserException {
-    Response response = http.get(Map.of("word", word));
-    throwExceptionIfNotOkayHttpStatus(response);
-    String content = response.getContent();
+    /**
+     * Returns the definition for the given word
+     */
+    public String getDefinition(String word) throws IOException, ParserException {
+        Response response = http.get(Map.of("word", word));
+        throwExceptionIfNotOkayHttpStatus(response);
+        String content = response.getContent();
 
-    TextParser parser = new TextParser(new StringReader(content));
-    return parser.parse().get(word);
-  }
+        TextParser parser = new TextParser(new StringReader(content));
+        return parser.parse().get(word);
+    }
 
     public void addDictionaryEntry(String word, String definition) throws IOException {
-      Response response = http.post(Map.of("word", word, "definition", definition));
-      throwExceptionIfNotOkayHttpStatus(response);
+        Response response = http.post(Map.of("word", word, "definition", definition));
+        throwExceptionIfNotOkayHttpStatus(response);
     }
 
-  public void removeAllDictionaryEntries() throws IOException {
-      Response response = http.delete(Map.of());
-      throwExceptionIfNotOkayHttpStatus(response);
+    public void removeAllDictionaryEntries() throws IOException {
+        Response response = http.delete(Map.of());
+        throwExceptionIfNotOkayHttpStatus(response);
     }
 
     private void throwExceptionIfNotOkayHttpStatus(Response response) {
-      int code = response.getHttpStatusCode();
-      if (code != HTTP_OK) {
-        String message = response.getContent();
-        throw new RestException(code, message);
-      }
+        int code = response.getHttpStatusCode();
+        if(code != HTTP_OK) {
+            String message = response.getContent();
+            throw new RestException(code, message);
+        }
     }
 
 }
