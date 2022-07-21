f11ac7d37a276d0dfa034e550e1f4f2c69dd3705
Alana Gilston <abluescarab@users.noreply.github.com>
2022-07-20 18:58:20 -0700

    Replaced TextDumper and PrettyPrinter with ones from project 3

--- CS410P-advprogramminginjava/phonebill-web/src/main/java/edu/pdx/cs410J/agilston/PrettyPrinter.java
+++ CS410P-advprogramminginjava/phonebill-web/src/main/java/edu/pdx/cs410J/agilston/PrettyPrinter.java
@@ -1,43 +1,63 @@
 package edu.pdx.cs410J.agilston;
 
-import com.google.common.annotations.VisibleForTesting;
+import edu.pdx.cs410J.PhoneBillDumper;
 
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.util.Map;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class PrettyPrinter {
+public class PrettyPrinter implements PhoneBillDumper<PhoneBill> {
     private final Writer writer;
 
     public PrettyPrinter(Writer writer) {
         this.writer = writer;
     }
 
-    @VisibleForTesting
-    static String formatWordCount(int count) {
-        return String.format("Dictionary on server contains %d words", count);
-    }
+    @Override
+    public void dump(PhoneBill bill) {
+        try(PrintWriter pw = new PrintWriter(this.writer)) {
+            StringBuilder line;
+            Duration duration;
+            long hours;
+            long minutes;
+            List<PhoneCall> calls = new ArrayList<>(bill.getPhoneCalls());
+            Collections.sort(calls);
 
-    @VisibleForTesting
-    static String formatDictionaryEntry(String word, String definition) {
-        return String.format("  %s : %s", word, definition);
-    }
+            pw.println("Phone bill for " + bill.getCustomer() + ":" + System.lineSeparator());
+            pw.println("CALLER       | CALLEE       | BEGIN TIME        | END TIME          | DURATION");
 
-    public void dump(Map<String, String> dictionary) {
-        try(
-                PrintWriter pw = new PrintWriter(this.writer)
-        ) {
+            for(PhoneCall call : calls) {
+                duration = Duration.ofMinutes(call.getDuration());
+                hours = duration.toHours();
+                minutes = duration.toMinutesPart();
+                line = new StringBuilder(call.getCaller());
 
-            pw.println(formatWordCount(dictionary.size()));
+                line.append(" | ")
+                    .append(call.getCallee())
+                    .append(" | ")
+                    .append(call.getBeginTimeString())
+                    .append(" | ")
+                    .append(call.getEndTimeString())
+                    .append(" | ");
 
-            for(Map.Entry<String, String> entry : dictionary.entrySet()) {
-                String word = entry.getKey();
-                String definition = entry.getValue();
-                pw.println(formatDictionaryEntry(word, definition));
+                if(hours > 0) {
+                    line.append(hours)
+                        .append(" ")
+                        .append(hours == 1 ? "hour" : "hours")
+                        .append(" ");
+                }
+
+                line.append(minutes)
+                    .append(" ")
+                    .append(minutes == 1 ? "minute" : "minutes");
+
+                pw.println(line);
             }
 
             pw.flush();
         }
-
     }
 }
