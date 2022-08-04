2b457bc393c7aa4c7457c5966f4dbee2077547d7
Alana Gilston <abluescarab@users.noreply.github.com>
2022-08-03 23:15:18 -0700

    Created activity with toolbar

--- CS410P-advprogramminginjava/phonebill-android/app/src/main/java/edu/pdx/cs410J/agilston/phonebill/MainActivity.java
+++ CS410P-advprogramminginjava/phonebill-android/app/src/main/java/edu/pdx/cs410J/agilston/phonebill/MainActivity.java
@@ -1,14 +1,67 @@
 package edu.pdx.cs410J.agilston.phonebill;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import edu.pdx.cs410J.agilston.phonebill.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this,
+                R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action",
+                                                               Snackbar.LENGTH_LONG)
+                                                       .setAction("Action", null).show());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+//        switch(item.getItemId()) {
+//            case R.id.action_help:
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
     }
 }
