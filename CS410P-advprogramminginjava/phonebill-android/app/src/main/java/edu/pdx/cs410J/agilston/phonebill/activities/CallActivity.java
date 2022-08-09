69378036a68afa029be18b8a605d3c7aba5b4719
Alana Gilston <abluescarab@users.noreply.github.com>
2022-08-09 08:34:14 -0700

    Call finish() for CallActivity up button

--- CS410P-advprogramminginjava/phonebill-android/app/src/main/java/edu/pdx/cs410J/agilston/phonebill/activities/CallActivity.java
+++ CS410P-advprogramminginjava/phonebill-android/app/src/main/java/edu/pdx/cs410J/agilston/phonebill/activities/CallActivity.java
@@ -33,6 +33,12 @@ public class CallActivity extends AppCompatActivity {
     private EditText editEndTime;
 
     @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
