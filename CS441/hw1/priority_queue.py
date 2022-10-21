5cda4fd554ae0c3291918b31bb6fb3f2c8cb116d
Alana Gilston <abluescarab@users.noreply.github.com>
2022-10-20 20:01:52 -0700

    Made PriorityQueue.items private

--- CS441/hw1/priority_queue.py
+++ CS441/hw1/priority_queue.py
@@ -3,23 +3,23 @@ from heapq import heappush, heappop
 
 class PriorityQueue:
     def __init__(self, *items):
-        self.items = []
+        self._items = []
 
         if items:
             for item in items:
                 self.push(item)
 
     def __str__(self):
-        return self.items.__str__()
+        return self._items.__str__()
 
     def __len__(self):
-        return len(self.items)
+        return len(self._items)
 
     def __contains__(self, item):
-        return item in self.items
+        return item in self._items
 
     def push(self, item):
-        heappush(self.items, item)
+        heappush(self._items, item)
 
     def pop(self):
-        return heappop(self.items)
+        return heappop(self._items)
