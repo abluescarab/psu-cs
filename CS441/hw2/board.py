eea147254175932d561749f771095dfb69aed819
Alana Gilston <abluescarab@users.noreply.github.com>
2022-11-09 02:09:17 -0800

    Implemented _reproduce(); changed state representation

--- CS441/hw2/board.py
+++ CS441/hw2/board.py
@@ -7,21 +7,7 @@ class Board:
         self.states = []
 
     def _generate_state(self):
-        # 0 = no queen present, 1 = queen present
-        state = [0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0,
-                 0, 0, 0, 0, 0, 0, 0, 0]
-        queens = random.sample(range(64), 8)
-
-        for queen in queens:
-            state[queen] = 1
-
-        return state
+        return random.sample(range(8), 8)
 
     # function Reproduce(x, y) returns an individual
     #     inputs: x, y, parent individuals
@@ -29,38 +15,9 @@ class Board:
     #     n <- Length(x); c <- random number from 1 to n
     #     return Append(Substring(x, 1, c), Substring(y, c + 1, n))
     def _reproduce(self, x, y):
-        x_count = 0
-
-        # ensure that there is at least one queen from x and y in the result
-        while x_count == 0 or x_count == 8:
-            c = random.choice(range(64))
-            result = x[:c]
-            x_count = sum(result)
-
-        # x_count = 0
-        # result = []
-        # tries = 100
-
-        # # ensure that 8 queens are always present after slicing; if unable to
-        # # find a solution after multiple slices, return failure
-        # while sum(result) < 8 and tries > 0:
-        #     # ensure that there is at least one queen from x in the result
-        #     while x_count == 0 or x_count == 8:
-        #         c = random.choice(range(64))
-        #         result = x[:c] + [0] * (64 - c)
-        #         x_count = sum(result)
-
-        #     # get the positions of queens from y
-        #     y_queens = [i for i, v in enumerate(y) if v == 1]
-
-        #     # apply queen positions from y to result until 8 queens total
-        #     for i in range(x_count, 8):
-        #         result[y_queens[i]] = 1
-
-        #     tries -= 1
-
-        # print(sum(result))
-        # return result
+        # ensure at least one element from x and y
+        c = random.randint(1, 7)
+        return x[:c] + y[c:]
 
     def populate(self, population):
         state = None
