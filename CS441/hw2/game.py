b1edfb7967a222f637ef3dcc3c72b863fae5cabb
Alana Gilston <abluescarab@users.noreply.github.com>
2022-11-09 04:06:57 -0800

    Passes population instead of using self.states

--- CS441/hw2/game.py
+++ CS441/hw2/game.py
@@ -6,33 +6,50 @@ class Game:
     def _generate_state(self):
         return random.sample(range(8), 8)
 
-    def _select_states(self, count):
-        if count > len(self.states):
-            count = len(self.states)
+    def _select_states(self, population, count):
+        if count > len(population):
+            count = len(population)
 
-        return [self.states.peek(i)[1] for i in range(count)]
+        return [population.peek(i)[1] for i in range(count)]
 
     def _reproduce(self, x, y):
         # ensure at least one element from x and y
         c = random.randint(1, 7)
         return x[:c] + y[c:]
 
+    def _mutate(self, state):
+        pass
 
-    def run(self, fitness_fn, population, max_cycles):
-        self.states = StateQueue(fitness_fn)
+    # function Genetic-Algorithm(population, Fitness-Fn) returns an individual
+    #     inputs: population, a set of individuals
+    #             Fitness-Fn, a function that measures fitness of individual
 
-        while len(self.states) < population:
+    #     repeat
+    #         new_population <- empty set
+    #         for i = 1 to Size(population) do
+    #             x <- Random-Selection(population, Fitness-Fn)
+    #             y <- Random-Selection(population, Fitness-Fn)
+    #             child <- Reproduce(x, y)
+    #             if small random probability then child <- Mutate(child)
+    #             add child to new_population
+    #         population <- new_population
+    #     until some individual is fit enough, or enough time has elapsed
+    #     return the best individual in population, according to Fitness-Fn
+    def run(self, fitness_fn, population_size, max_cycles):
+        population = StateQueue(fitness_fn)
+
+        while len(population) < population_size:
             state = self._generate_state()
 
-            if state not in self.states:
-                self.states.push(state)
+            if state not in population:
+                population.push(state)
 
         cycle = 0
 
-        while cycle < max_cycles and self.states.peek()[0] != 0:
+        while cycle < max_cycles and population.peek()[0] != 0:
             x, y = self._select_states(2)
             child = self._reproduce(x, y)
-            self.states.push(child)
+            population.push(child)
             cycle += 1
 
-        return self.states.pop()
+        return population.pop()
