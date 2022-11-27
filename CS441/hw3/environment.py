d686a9ddec763739b15ddd3b719f6487d50b3600
Alana Gilston <abluescarab@users.noreply.github.com>
2022-11-26 21:10:24 -0800

    Environment checks terminality, finds next square, checks terminality

--- CS441/hw3/environment.py
+++ CS441/hw3/environment.py
@@ -22,16 +22,41 @@ class Environment:
         elif action == Action.West:
             return square % 10 == 0
 
-    def _calculate_reward(self, state, square: int, action: Action):
+    def _get_next_square(self, square, action: Action):
+        if action == Action.Grab or self._has_wall(square, action):
+            return square
+        elif action == Action.North:
+            return square - 10
+        elif action == Action.South:
+            return square + 10
+        elif action == Action.East:
+            return square + 1
+        elif action == Action.West:
+            return square - 1
+
+    def _calculate_reward(self, state, action: Action):
+        square = self._get_next_square(self.robby, action)
+
         if action == Action.Grab:
             return 10 if state[square] else -1
         else:
             return -5 if self._has_wall(square, action) else 0
 
-    def _generate(self):
-        state = []
+    def _is_terminal(self):
+        return not any(self.state)
+
+    def generate(self):
+        self.state = []
 
         for _ in range(100):
-            state.append(random.choices([0, 1], [0.5, 0.5])[0])
+            self.state.append(random.choices([0, 1], [0.5, 0.5])[0])
 
-        self.state, self.robby = state, random.randint(0, 99)
+        self.robby = random.randint(0, 99)
+
+    def reward_signal(self, state):
+        actions = {}
+
+        for action in Action:
+            actions[action] = self._calculate_reward(state, action)
+
+        return max(actions, key=actions.get)
