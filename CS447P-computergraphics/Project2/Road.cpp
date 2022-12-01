7c9c0d967b436b1474bb5d58c0fbf0f59277fe38
Alana Gilston <abluescarab@users.noreply.github.com>
2022-12-01 00:11:33 -0800

    Moved TexturedQuadStrip to Road class

--- CS447P-computergraphics/Project2/Road.cpp
+++ CS447P-computergraphics/Project2/Road.cpp
@@ -1,6 +1,43 @@
 #include "Road.h"
 #include "Point3D.h"
-#include "utils.h"
+
+void Road::TexturedQuadStrip(Point3D& left, Point3D& right, Point3D offset,
+    int count, bool stretch, bool moveLeft, bool moveRight) {
+    GLfloat ty = 0.0;
+    int i = 0;
+
+    for(int i = 0; i <= count; i++) {
+        if(i > 0) {
+            if(stretch) {
+                if(count - 1 == 0) {
+                    ty = 1.0;
+                }
+                else {
+                    ty = (float)i / (float)count;
+                }
+            }
+            else {
+                ty = (ty == 0.0 ? 1.0 : 0.0);
+            }
+        }
+
+        glTexCoord2f(0.0, ty);
+        glVertex3f(left.x, left.y, left.z);
+
+        glTexCoord2f(1.0, ty);
+        glVertex3f(right.x, right.y, right.z);
+
+        if(i < count) {
+            if(moveLeft) {
+                left += offset;
+            }
+
+            if(moveRight) {
+                right += offset;
+            }
+        }
+    }
+}
 
 bool Road::Initialize() {
     return TexturedObject::Initialize("road.tga");
@@ -12,13 +49,13 @@ void Road::InitializeDisplay() {
     
     glNormal3f(0.0, 0.0, 1.0);
     glBegin(GL_QUAD_STRIP);
-    utils::TexturedQuadStrip(left, right, Point3D(0.0, 1.0, 0.0), 39, false);
-    utils::TexturedQuadStrip(left, right, Point3D(0.0, 1.0, 0.0), 2, false, true, false);
-    utils::TexturedQuadStrip(left, right, Point3D(1.0, 0.0, 0.0), 39, false);
-    utils::TexturedQuadStrip(left, right, Point3D(1.0, 0.0, 0.0), 2, false, true, false);
-    utils::TexturedQuadStrip(left, right, Point3D(0.0, -1.0, 0.0), 39, false);
-    utils::TexturedQuadStrip(left, right, Point3D(0.0, -1.0, 0.0), 2, false, true, false);
-    utils::TexturedQuadStrip(left, right, Point3D(-1.0, 0.0, 0.0), 39, false);
-    utils::TexturedQuadStrip(left, right, Point3D(-1.0, 0.0, 0.0), 2, false, true, false);
+    Road::TexturedQuadStrip(left, right, Point3D(0.0, 1.0, 0.0), 39, false);
+    Road::TexturedQuadStrip(left, right, Point3D(0.0, 1.0, 0.0), 2, false, true, false);
+    Road::TexturedQuadStrip(left, right, Point3D(1.0, 0.0, 0.0), 39, false);
+    Road::TexturedQuadStrip(left, right, Point3D(1.0, 0.0, 0.0), 2, false, true, false);
+    Road::TexturedQuadStrip(left, right, Point3D(0.0, -1.0, 0.0), 39, false);
+    Road::TexturedQuadStrip(left, right, Point3D(0.0, -1.0, 0.0), 2, false, true, false);
+    Road::TexturedQuadStrip(left, right, Point3D(-1.0, 0.0, 0.0), 39, false);
+    Road::TexturedQuadStrip(left, right, Point3D(-1.0, 0.0, 0.0), 2, false, true, false);
     glEnd();
 }
--- CS447P-computergraphics/Project2/Road.h
+++ CS447P-computergraphics/Project2/Road.h
@@ -3,8 +3,14 @@
 
 #include <Fl/gl.h>
 #include "TexturedObject.h"
+#include "Point3D.h"
 
 class Road : public TexturedObject {
+private:
+    static void TexturedQuadStrip(Point3D& left, Point3D& right, Point3D offset,
+        int count, bool stretch = false, bool moveLeft = true,
+        bool moveRight = true);
+
 public:
     bool Initialize();
     void InitializeDisplay();
